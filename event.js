f43717e5e4909ba39bf365cb0866f3b40cd66132
njaison <nidhijaison@gmail.com>
2018-12-01 20:58:21 -0500

    Formatting

--- event.js
+++ event.js
@@ -1,6 +1,6 @@
 "use strict";
 const Alexa = require('alexa-sdk');
- var volunteer = require('./volunteer')
+var volunteer = require('./volunteer')
 
 var fullEvent = [];
 
@@ -8,16 +8,16 @@ var myEvent = "";
 var myHours = 0;
 var myDate = "";
 
-exports.addEvent = function (event, hours, date){
+exports.addEvent = function (event, hours, date) {
     myEvent = event
     myHours = hours
     myDate = date
     this.toArray();
     volunteer.addToFullList(fullEvent)
-   console.log("Loaded event")
+    console.log("Loaded event")
 }
 
-exports.toArray = function(){
+exports.toArray = function () {
     fullEvent.push(myEvent)
     fullEvent.push(myHours.toString())
     fullEvent.push(myDate)
--- volunteer.js
+++ volunteer.js
@@ -3,38 +3,38 @@ const Alexa = require('alexa-sdk');
 
 var allEvents = [];
 
-exports.addToFullList = function(event){
+exports.addToFullList = function (event) {
     allEvents.push(event)
 }
 
-exports.clear = function(){
-    if(allEvents.length != 0){
+exports.clear = function () {
+    if (allEvents.length != 0) {
         allEvents.length = 0;
-    }else{
-        this.emit(":ask", "List already empty!","List already empty!")
+    } else {
+        this.emit(":ask", "List already empty!", "List already empty!")
     }
 
 
 }
 
-exports.getTotalHours = function(){
+exports.getTotalHours = function () {
     var i;
     var total = 0;
-    
-    if(allEvents.length == 0){
+
+    if (allEvents.length == 0) {
         return "zero"
-    }else{
-        for (i = 0; i < allEvents.length; i++) { 
-        let hours = parseInt(allEvents[i][1])
-        total += hours;
+    } else {
+        for (i = 0; i < allEvents.length; i++) {
+            let hours = parseInt(allEvents[i][1])
+            total += hours;
         }
-    return total;
+        return total;
     }
 }
 
-exports.getLastEvent = function(){
-    return allEvents[allEvents.length-1]
+exports.getLastEvent = function () {
+    return allEvents[allEvents.length - 1]
 }
- exports.getNumberOfEvents = function(){
-     return allEvents.length
- }
+exports.getNumberOfEvents = function () {
+    return allEvents.length
+}
